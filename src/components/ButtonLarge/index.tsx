8e3a8663361e70e07d191a0cfd3dc0f58ab012eb
Caio Luiz Cardoso Oliveira <ccardoso210@gmail.com>
2023-01-20 20:37:35 -0300

    botão entrar

--- src/components/ButtonLarge/index.tsx
+++ src/components/ButtonLarge/index.tsx
@@ -2,14 +2,11 @@ import * as S from "./style";
 import React, { ButtonHTMLAttributes } from "react";
 type ButtonType = ButtonHTMLAttributes<HTMLButtonElement>;
 type ButtonLargeProps = {
-    value: string;
-  } & ButtonType;
-  
+  value: string;
+} & ButtonType;
 
-const ButtonLarge = () => {
-  return (
-    <S.ButtonLarge></S.ButtonLarge>
-  );
-}
+const ButtonLarge = ({ value, ...props }: ButtonLargeProps) => {
+  return <S.ButtonLarge {...props}>{value}</S.ButtonLarge>;
+};
 
 export default ButtonLarge;
