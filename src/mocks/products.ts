cc57c767692713826f5c19eed24b2e030371b6da
Caio Luiz Cardoso Oliveira <ccardoso210@gmail.com>
2023-01-21 15:55:13 -0300

    products

--- src/mocks/products.ts
+++ src/mocks/products.ts
@@ -1,20 +1,20 @@
 export const products = [
   {
-    id: "507f191e810c19729de860ea",
+    id: "12354678",
     name: "Cabine de Fotos",
     description: "Aluguel de cabine de fotos para eventos",
     image: "/assets/images/produtos/cabine-de-fotos.jpg",
     price: 199.99,
   },
   {
-    id: "507f191e810c19729de860ea",
+    id: "12354678",
     name: "Cabine de Fotos",
     description: "Aluguel de cabine de fotos para eventos",
     image: "/assets/images/produtos/cabine-de-fotos.jpg",
     price: 199.99,
   },
   {
-    id: "507f191e810c19729de860ea",
+    id: "12354678",
     name: "Cabine de Fotos",
     description: "Aluguel de cabine de fotos para eventos",
     image: "/assets/images/produtos/cabine-de-fotos.jpg",
